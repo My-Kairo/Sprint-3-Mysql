2c5b5a9df8025c1d521aaf513bd201938d048b6b
My-Kairo <51219497+My-Kairo@users.noreply.github.com>
2020-02-19 10:51:46 +0200

    Add files via upload

---
+++ myySQL.sql
@@ -0,0 +1,51 @@
+"Food Type",Items,Size,"Selling Price(R)",Quantity,"Stock price(R)",Profit(R),Sales,Bar_code,Shelve_No,packing_no
+Chips,Simba,125g,14,15,196,210,22,,,NULL
+Chips,Lays,125g,15,10,135,150,9,,,NULL
+Chips,Doritos,125g,16,12,176,192,7,,,NULL
+Chips,"Nick Nacks",50g,1,40,39,40,29,,,NULL
+Chips,Bigga,50g,1,50,49,50,41,,,NULL
+Chips,Crack-A-Snacks,50g,1,50,49,50,37,,,NULL
+CoolDrink,Coke,2lt,18,22,378,396,17,,,NULL
+Cooldrink,"Fanta Pine",2lt,17,25,408,425,19,,,NULL
+Cooldrink,"Fanta Orange",2lt,16,23,352,368,16,,,NULL
+Cooldrink,"Granadilla Twist",2lt,20,20,380,400,21,,,NULL
+Cooldrink,"Lemon Twist",2lt,18,25,432,450,20,,,NULL
+Cooldrinks,Shweps,2lt,20,25,480,500,20,,,NULL
+Cooldrink,Spar-Leta,2lt,18,25,432,450,22,,,NULL
+Chocolate,Cadbury,55g,10,25,240,250,19,,,NULL
+Chocolate,PS,55g,12,15,168,180,12,,,NULL
+Chocolate,"Lunch Bar",55g,9,20,171,180,17,,,NULL
+Chocolate,"TV Bar",55g,9,20,171,180,15,,,NULL
+Chocolate,Tex,55g,10,25,240,250,21,,,NULL
+Chocolate,Kit-Kat,55g,8,30,232,240,24,,,NULL
+Chocolate,Lindlt,55g,12,25,288,300,20,,,NULL
+Pie,"Pepper Steak",100g,20,20,380,400,17,,,NULL
+Pie,"Chicken and mashroom",100g,22,35,748,770,29,,,NULL
+Pie,"Steak and kidney",100g,21,30,609,630,25,,,NULL
+Pie,Chicken,100g,19,30,551,570,27,,,NULL
+Fruit,Pear,1kg,2,70,138,140,65,,,NULL
+Fruit,Apple,1kg,3,80,237,240,74,,,NULL
+Fruit,Orange,1kg,3,60,177,180,56,,,NULL
+Fruit,Kiwi,1kg,3,65,192,195,54,,,NULL
+Fruit,Mango,1kg,5,75,370,375,67,,,NULL
+Fruit,Banana,1kg,2,85,168,170,72,,,NULL
+Fruit,Paw-paw,1kg,4,50,196,200,41,,,NULL
+Fruit,Pine-apple,1kg,7,45,308,315,36,,,NULL
+Cupcakes,Vanilla,70g,8,35,272,280,29,,,NULL
+Cupcakes,Chocolate,70g,7,48,329,336,39,,,NULL
+Cupcakes,Bran,70g,6,50,294,300,44,,,NULL
+Cupcakes,Cappucino,70g,7,25,168,175,19,,,NULL
+Veg,Spinach,1kg,10,25,240,250,18,,,NULL
+Veg,carrots,1kg,6,23,132,138,20,,,NULL
+Veg,Potatoes,1kg,7,27,182,189,23,,,NULL
+Veg,Beans,1kg,8,30,232,240,21,,,NULL
+Veg,Corn,1kg,9,40,351,360,33,,,NULL
+Veg,Beetroot,1kg,12,45,528,540,39,,,NULL
+Veg,Butternut,1kg,15,40,585,600,34,,,NULL
+Juices,Fusion,700ml,8,30,232,240,27,,,NULL
+Juices,Oros,750ml,17,25,408,425,19,,,NULL
+Juices,Halls,1lt,17,34,561,578,27,,,NULL
+Juices,Appletiser,500ml,23,30,667,690,25,,,NULL
+Juices,Ceres,440ml,20,40,780,800,32,,,NULL
+Juices,Liqior-Fruit,1lt,20,35,680,700,30,,,NULL
+Juices,Crush,750ml,24,30,696,720,21,,,NULL
